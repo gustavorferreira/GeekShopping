03333191883269a9a73b82a1c3015769476e68a8
TAKENET\gustavo.ferreira <gustavo.ferreira@take.net>
2022-06-18 18:28:41 -0300

    add partial login and resource Login and Logout

--- src/GeekShopping.WebApplication/Controllers/HomeController.cs
+++ src/GeekShopping.WebApplication/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
 ﻿using GeekShopping.WebApplication.Models;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using System.Diagnostics;
 
@@ -27,6 +28,17 @@ namespace GeekShopping.WebApplication.Controllers
         public IActionResult Error()
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+
+        [Authorize]
+        public async Task<IActionResult> Login()
+        {
+            return RedirectToAction(nameof(Index));
+        }
+
+        public IActionResult Logout()
+        {
+            return SignOut("Cookies", "oidc");
         }
     }
 }
